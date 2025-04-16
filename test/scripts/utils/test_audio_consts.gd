027e109b0bcbcdf9c142c249d0fd1d53cc3ef477
Jon Alc <jon.f.alcantara@gmail.com>
2025-04-16 07:35:15 -0700

    Adds audio slider tests

---
+++ test/scripts/utils/test_audio_consts.gd
@@ -0,0 +1,15 @@
+class_name TestAudioConsts
+
+#region Variables
+## The path to the expected output json for test__audio_master().
+const TEST_AUDIO_MASTER_EXP_PATH : String = \
+	"res://test/data/settings/settings_test_audio/test__audio_master/original_output.json"
+
+## The path to the expected output json for test__audio_music().
+const TEST_AUDIO_MUSIC_EXP_PATH : String = \
+	"res://test/data/settings/settings_test_audio/test__audio_music/original_output.json"
+
+## The path to the expected output json for test__audio_sfx().
+const TEST_AUDIO_SFX_EXP_PATH : String = \
+	"res://test/data/settings/settings_test_audio/test__audio_sfx/original_output.json"
+#endregion
